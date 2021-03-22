1c559a67e95cc1ff5f67fbb818b2c4cba6cadce9
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-03-21 21:38:51 -0400

    idk lets get some data in

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import allData from './example-data.json';
 const App = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    if (data === null) setData(allData);
-  }, []);
+    if (data === null) setData(allData.data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
   console.log(data)
   return (
     <div className="App">
-      <table>
-        <thead>
-          {Object.keys(data).map(key => <td>{key}</td>)}
-        </thead>
-        <tbody>
-          {Object.map(
-            obj => <tr>
-              {Object.values(obj).map(val => <td>{obj[val]}:{val}</td>)}
-              </tr>)}
-        </tbody>
-      </table>
+      {data === null
+      ? <div>Loading...</div>
+      : <table>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map(key => <td>{key}</td>)}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(
+              obj => <tr>
+                {Object.values(obj).map(val => <td>{obj[val]}:{val}</td>)}
+                </tr>)}
+          </tbody>
+        </table>
+      }
     </div>
   );
 }
